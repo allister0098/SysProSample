ed72189bd89ffb4243ecb181d662dfcac5471698
shotaro <s1210098@gmail.com>
2021-02-27 19:33:01 +0900

    [add]readvのサンプル追加

--- Makefile
+++ Makefile
@@ -3,5 +3,4 @@ CFLAGS := -Wall -Wextra -O2 -g
 
 poll: poll/poll_example
 writev: scatter-gather-io/writev
-
-# gcc -Wall -Wextra -O2 -o -g poll_example poll_example.c
+readv: scatter-gather-io/readv
---
+++ scatter-gather-io/readv.c
@@ -0,0 +1,47 @@
+#include <stdio.h>
+#include <sys/types.h>
+#include <sys/stat.h>
+#include <fcntl.h>
+#include <sys/uio.h>
+#include <unistd.h>
+
+/**
+ * 1つのファイルの内容を複数のセグメントへ読みこむサンプル
+ */
+int main() {
+    char foo[47], bar[50], baz[48];
+    struct iovec iov[3];
+    ssize_t nr;
+    int fd, i;
+
+    fd = open("baccaneer.txt", O_RDONLY);
+    if (fd == -1) {
+        perror("open");
+        return 1;
+    }
+
+   iov[0].iov_base = foo;
+   iov[0].iov_len =  sizeof(foo);
+   iov[1].iov_base = bar;
+   iov[1].iov_len =  sizeof(bar);
+   iov[2].iov_base = baz;
+   iov[2].iov_len =  sizeof(baz);
+
+    /* 一度のシステムコールで３つのセグメントへファイルの内容を読み込む */
+    nr = readv(fd, iov, 3);
+    if (nr == -1) {
+        perror("readv");
+        return 1;
+    }
+
+    for (i = 0; i < 3; i++) {
+        printf("%d: %.*s\n", i, iov[i].iov_len, (char *) iov[i].iov_base);
+    }
+
+    if (close(fd)) {
+        perror("close");
+        return 1;
+    }
+
+    return 0;
+}
